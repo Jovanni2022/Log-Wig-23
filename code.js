8aaedad5634a36e9d6e6758e35d45ab428f9c87a
Jovanni2022 <116310309+Jovanni2022@users.noreply.github.com>
2022-12-13 15:14:57 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+function hello(name) {
+    alert("hello " + name);
+}
+hello("Can you find the secret message");
+
+function secretMessage(message){
+console.log("The secret message is" + message);
+}
+secretMessage("Call me an orange, because I am juiced");
